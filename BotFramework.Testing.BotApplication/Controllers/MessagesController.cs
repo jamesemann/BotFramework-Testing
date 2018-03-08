e8ac04c179c258b5b0a0c9c385f00bb67b788766
James Mann <james@blackmarble.com>
2018-03-08 07:41:46 -0800

    Add project files.

---
+++ BotFramework.Testing.BotApplication/Controllers/MessagesController.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BotFramework.Testing.Middleware;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Adapters;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Bot.Samples.Ai.QnA.Controllers
+{
+    [Route("api/[controller]")]
+    public class MessagesController : Controller
+    {
+        
+        static BotFrameworkAdapter adapter;
+
+        public MessagesController(IConfiguration configuration)
+        {
+            if (adapter == null)
+            {
+                adapter = new BotFrameworkAdapter(new ConfigurationCredentialProvider(configuration))
+                    .Use(new MyFirstMiddleware());
+            }
+        }
+
+        private Task BotReceiveHandler(IBotContext context)
+        {
+            context.Reply("hello");
+            return Task.CompletedTask;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Activity activity)
+        {
+            try
+            {
+                await adapter.ProcessActivity(this.Request.Headers["Authorization"].FirstOrDefault(), activity, BotReceiveHandler);
+                return this.Ok();
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return this.Unauthorized();
+            }
+        }
+    }
+}
---
+++ BotFramework.Testing.Middleware/MyFirstMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Middleware;
+using Microsoft.Bot.Schema;
+
+namespace BotFramework.Testing.Middleware
+{
+    public class MyFirstMiddleware : IReceiveActivity, ISendActivity, IContextCreated
+    {
+        public MyFirstMiddleware()
+        {
+        }
+
+        public async Task ContextCreated(IBotContext context, MiddlewareSet.NextDelegate next)
+        {
+            context.Reply("ContextCreated");
+            await next().ConfigureAwait(false);
+        }
+
+        public async Task ReceiveActivity(IBotContext context, MiddlewareSet.NextDelegate next)
+        {
+            context.Reply("ReceiveActivity");
+            await next().ConfigureAwait(false);
+        }
+
+        public async Task SendActivity(IBotContext context, IList<Activity> activities, MiddlewareSet.NextDelegate next)
+        {
+            context.Reply("SendActivity");
+            await next().ConfigureAwait(false);
+        }
+    }
+}
